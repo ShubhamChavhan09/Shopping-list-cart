1cce4f9220762da22354ccd793bb30a862c5e0c9
Shubham <shubh.chavhan00@gmail.com>
2021-09-05 19:43:38 +0530

    created a component to show multiple counter to add or delete it

---
+++ src/components/counters.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+export default class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 3 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    const count = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: count });
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((c) => (
+          <Counter
+            key={c.id}
+            value={c.value}
+            onDelete={this.handleDelete}
+            id={c.id}
+          />
+        ))}
+      </div>
+    );
+  }
+}
