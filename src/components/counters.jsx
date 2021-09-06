299c0fd4e9ed6521b8a54716856b602a2dfdeb77
Shubham <shubh.chavhan00@gmail.com>
2021-09-06 19:46:32 +0530

    removed all the event handlers and the state to add to the parent element

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,46 +2,17 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 export default class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 3 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleDelete = (counterId) => {
-    const count = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters: count });
-  };
   render() {
     return (
       <div>
-        <button onClick={this.handleReset} className="btn">
+        <button onClick={this.props.onReset} className="btn btn-blue">
           Reset
         </button>
-        {this.state.counters.map((c) => (
+        {this.props.counters.map((c) => (
           <Counter
             key={c.id}
-            onIncrement={this.handleIncrement}
-            onDelete={this.handleDelete}
+            onIncrement={this.props.onIncrement}
+            onDelete={this.props.onDelete}
             counter={c}
           />
         ))}
